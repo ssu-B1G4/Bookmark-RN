65470f5c20125da158e59062366d9333a0ab2620
solmee <kii5541@naver.com>
2025-02-02 06:18:23 +0900

    feat: 위치 권한 설정 추가

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
+  PermissionsAndroid,
+  Platform,
   SafeAreaView,
   StatusBar,
   StyleSheet,
@@ -8,6 +10,29 @@ import {
 import WebView from 'react-native-webview';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
+const requestLocationPermission = async () => {
+  if (Platform.OS === 'ios') {
+    return true;
+  }
+
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: '위치 권한 필요',
+        message: '지도 표시를 위해 위치 권한이 필요합니다.',
+        buttonNeutral: '나중에',
+        buttonNegative: '취소',
+        buttonPositive: '확인',
+      },
+    );
+    return granted === PermissionsAndroid.RESULTS.GRANTED;
+  } catch (err) {
+    console.warn(err);
+    return false;
+  }
+};
+
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -15,6 +40,10 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     flex: 1,
   };
+
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -29,6 +58,7 @@ function App(): React.JSX.Element {
         domStorageEnabled={true}
         startInLoadingState={true}
         scalesPageToFit={true}
+        geolocationEnabled={true}
       />
     </SafeAreaView>
   );
